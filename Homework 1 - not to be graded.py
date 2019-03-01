474830cb62ca01ca36f94bf4f10b46d339a66b19
berilbeler <47668317+berilbeler@users.noreply.github.com>
2019-03-01 11:14:18 +0300

    Add files via upload

---
+++ Homework 1 - not to be graded.py
@@ -0,0 +1,7 @@
+#in class assignment
+integer1 = float (input ("Please enter integer 1"))
+integer2 = float (input ("Please enter integer 2"))
+integer3 = float (input ("Please enter integer 3"))
+summ = integer1 + integer2 + integer3
+avg = (integer1 + integer2 + integer3)/ 3
+print("sum of the numbers is ", summ, "avarage of the numbers is ", avg)
